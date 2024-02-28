fdcd514c1959d13fba46800530b9fb051112e657
Mih53 <mih53@4146c987a9a6>
2024-02-28 22:35:25 +0000

    Aula03

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,16 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def hello_word():
+    return  render_template('index.html')
+
+@app.route('/identificacao')
+def identificacao():
+    return  render_template('identificacao.html')
+
+@app.route('/contextodarequisicao')
+def contextodarequisicao():
+    return  render_template('contextodarequisicao.html')
 
